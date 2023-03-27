96c95264f32f2cc911d8aa0ca8fd2ebb1091f9f9
Matan Yosef <matan.yosef02@gmail.com>
2023-03-27 20:51:29 +1300

    PDF-10: refactored to use basic dark mode and structure
    Includes the initial stuff, as well as a button in the footer to test the dark/light switch functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BeakerIcon } from '@heroicons/react/24/solid' // e.g. Icon use
+import {useState} from "react";
 
+export default function App() {
 
-function App() {
-  const [count, setCount] = useState(0)
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-4xl shadow-xl m-4 p-4 border-2 border-red-200">Vite + React + Tailwind</h1>
-      <BeakerIcon className="h-6 w-6 text-blue-500"/>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    function toggleDarkMode() {
+        // TODO: Add the localStorage and systemPreference when we have users + their reference
+        isDarkMode ?   document.documentElement.classList.remove('dark') : document.documentElement.classList.add('dark');
+        setIsDarkMode(!isDarkMode);
+    }
+
+    return (
+        <div className="h-screen flex flex-col">
+            <div className="grow dark:bg-anno-space-700">
+
+                <p>Layout Component goes here (everything else goes inside layout)?</p>
+
+            </div>
+
+            <footer className="justify-self-end bg-anno-red-primary p-8 flex flex-col items-center justify-center items-center gap-4 dark:bg-anno-red-secondary">
+                <p className="text-white">&copy; Anno 2023 | The University of Auckland | New Zealand </p>
+
+                {/*TODO: Remove when we have users */}
+                <button className="bg-gray-200 text-black px-3 py-2 transition-colors hover:bg-blue-200 rounded" onClick={() => toggleDarkMode()}>Toggle Dark Mode: {isDarkMode ? 'DARK' : 'LIGHT'}</button>
+            </footer>
+        </div>
+    )
 }
-
-export default App
