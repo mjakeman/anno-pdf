2168d375beb58aa94caa9906ea33a382f48617c4
Matan Yosef <matan.yosef02@gmail.com>
2023-03-29 18:52:22 +1300

    PDF-15: Added Share Button
    Essentially serves as example usage.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
-import {useState} from "react";
+import React, {useState} from "react";
+import PrimaryButton from "./PrimaryButton";
+import {UserPlusIcon} from "@heroicons/react/24/outline";
 
 export default function App() {
 
@@ -16,6 +18,8 @@ export default function App() {
 
                 <p>Layout Component goes here (everything else goes inside layout)?</p>
 
+                {/*Share Button - TODO: Doesn't do anything at the moment*/}
+                <PrimaryButton label={"Share"} icon={<UserPlusIcon className={"h-6 w-6"} />} onClick={() => console.log('Clicked!')}/>
             </div>
 
             <footer className="justify-self-end bg-anno-red-primary p-8 flex flex-col items-center justify-center items-center gap-4 dark:bg-anno-red-secondary">
