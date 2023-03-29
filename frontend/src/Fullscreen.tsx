3c07d6e8294cdb51252891bc3b4bdf054df2b6cf
Jordan York <yorkjordan07@gmail.com>
2023-03-29 20:45:39 +1300

    PDF-12 - recommended changes added

--- frontend/src/Fullscreen.tsx
+++ frontend/src/Fullscreen.tsx
@@ -1,16 +1,17 @@
 import React from "react";
+import {ArrowsPointingOutIcon} from "@heroicons/react/20/solid";
 interface FullscreenButtonProps {
     onClick:  (params: any) => any;
     label: string,
     icon?: React.ReactNode,
 
 }
-export default function Fullscreen({onClick, label, icon} : FullscreenButtonProps){
+export default function Fullscreen({onClick, label} : FullscreenButtonProps){
     return (
         <button type="button"
                 onClick={onClick}>
             <span>{label}</span>
-            {icon}
+            <ArrowsPointingOutIcon className="h-7 w-7 p-1 border-2 rounded-full border-black" />
         </button>
     )
 }
