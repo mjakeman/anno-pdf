2f0689ad9cc3c0e1380b10aec757794cfbc4ccfa
Matan Yosef <matan.yosef02@gmail.com>
2023-04-01 00:30:37 +1300

    PDF-16: fixed padding in primary button

--- frontend/src/PrimaryButton.tsx
+++ frontend/src/PrimaryButton.tsx
@@ -11,7 +11,7 @@ export default function PrimaryButton({onClick, label, icon} : PrimaryButtonProp
     return (
         <button type="button"
                 onClick={onClick}
-                className={"bg-anno-red-primary p-2 px-4 text-white flex flex-row items-center content-center rounded-lg gap-1 text-lg transition-colors hover:bg-anno-red-secondary"}
+                className={"bg-anno-red-primary py-2 px-4 text-white flex flex-row items-center content-center rounded-lg gap-1 text-lg transition-colors hover:bg-anno-red-secondary"}
         >
             <span>{label}</span>
             {icon}
