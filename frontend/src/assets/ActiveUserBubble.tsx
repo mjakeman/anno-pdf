0476257fc43d54b94b3264eebd5cbe2ff659514f
Matan Yosef <matan.yosef02@gmail.com>
2023-04-01 16:29:18 +1300

    PDF-14: Created ActiveUserBubble
    Represents a singular active user (will be used in the full list).

---
+++ frontend/src/assets/ActiveUserBubble.tsx
@@ -0,0 +1,28 @@
+import ProfileBubble, {ProfileBubbleSizes} from "../ProfileBubble";
+import React, {useState} from "react";
+
+interface ActiveUserBubbleProps {
+    user: {id: number, fullName: string, email: string };
+}
+
+export default function ActiveUserBubble({user} : ActiveUserBubbleProps) {
+
+    const [showUserInfoCard, setShowUserInfoCard] = useState(false);
+
+    return (
+        <div className="-ml-4 relative">
+            <span onMouseOver={() => setShowUserInfoCard(true)} onMouseLeave={() => setShowUserInfoCard(false)} className="hover:cursor-pointer">
+                <ProfileBubble size={ProfileBubbleSizes.Large} fullName={user.fullName}></ProfileBubble>
+            </span>
+
+            {/* Shown only on hover... */}
+            <div className={`${!showUserInfoCard && "hidden"} absolute bg-white shadow px-6 py-4 rounded-xl flex flex-row items-center gap-2`}>
+                <ProfileBubble size={ProfileBubbleSizes.Small} fullName={user.fullName}/>
+                <div className="flex flex-col grow">
+                    <p className="font-medium">{user.fullName}</p>
+                    <span className="text-slate-500 text-sm">{user.email}</span>
+                </div>
+            </div>
+        </div>
+    );
+}
