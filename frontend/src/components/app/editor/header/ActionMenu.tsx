8de3ab16a764f1badc73ea88270b3309ff315773
Matan Yosef <matan.yosef02@gmail.com>
2023-04-04 22:33:25 +1200

    PDF-26: Dark mode sweep

--- frontend/src/components/app/editor/header/ActionMenu.tsx
+++ frontend/src/components/app/editor/header/ActionMenu.tsx
@@ -16,28 +16,29 @@ export default function ActionMenu({onDownload, onCopy, onDelete} : ActionMenuPr
 
     return (
         <div className="flex flex-row items-center">
-            <button onClick={() => setShowExpandedMenu(!showExpandedMenu)} type="button" className="z-50 bg-anno-red-primary w-8 h-8 flex items-center justify-center rounded-full transition-colors hover:bg-anno-red-secondary">
+            <button onClick={() => setShowExpandedMenu(!showExpandedMenu)} type="button" className="z-50 bg-anno-red-primary dark:bg-anno-red-secondary w-8 h-8 flex items-center justify-center rounded-full transition-colors hover:bg-anno-red-secondary dark:hover:bg-anno-pink">
                 {showExpandedMenu ? <XMarkIcon className="w-5 h-5 text-white" /> : <EllipsisHorizontalIcon className="w-8 h-8 text-white"/> }
             </button>
             {showExpandedMenu &&
-                <div className="-ml-4 flex rounded-r-xl border-y-2 border-r-2 border-anno-red-primary">
+                <div className="-ml-4 flex rounded-r-xl border-y-2 border-r-2 border-anno-red-primary dark:border-anno-red-secondary">
                     <div className="flex flex-row ml-6 mr-2 gap-2 items-stretch justify-center">
 
-                        <button onClick={onDownload} type="button" className="rounded-lg px-2 py-1 hover:bg-zinc-200">
-                            <ArrowDownTrayIcon className="w-5 h-5 text-anno-red-primary" />
+                        <button onClick={onDownload} type="button" className="rounded-lg px-2 py-1 hover:bg-zinc-200 dark:hover:bg-anno-space-700 transition-colors">
+                            <ArrowDownTrayIcon className="w-5 h-5 text-anno-red-primary dark:text-anno-pink" />
                         </button>
 
                         {/* TODO: maybe make this a component? */}
-                        <div className="border-l-2 border-anno-pink my-1 translate-x-1/2"></div>
+                        <div className="border-l-2 border-anno-pink dark:border-pink-100 my-1.5 translate-x-1/2"></div>
 
-                        <button onClick={onCopy} type="button" className="rounded-lg px-2 hover:bg-zinc-200">
-                            <DocumentDuplicateIcon className="w-4 h-4 text-anno-red-primary" />
+                        {/* TODO maybe maybe each option a component?*/}
+                        <button onClick={onCopy} type="button" className="rounded-lg px-2 hover:bg-zinc-200 dark:hover:bg-anno-space-700 transition-colors">
+                            <DocumentDuplicateIcon className="w-4 h-4 text-anno-red-primary dark:text-anno-pink" />
                         </button>
 
-                        <div className="border-l-2 border-anno-pink my-1 translate-x-1/2"></div>
+                        <div className="border-l-2 border-anno-pink dark:border-pink-100  my-1.5 translate-x-1/2"></div>
 
-                        <button onClick={onDelete}  type="button" className="rounded-lg px-2 hover:bg-red-100">
-                            <TrashIcon className="w-5 h-5 stroke-2 text-red-500" />
+                        <button onClick={onDelete}  type="button" className="rounded-lg px-2 hover:bg-red-100 dark:hover:bg-zinc-300 transition-colors">
+                            <TrashIcon className="w-5 h-5 stroke-2 text-red-500 dark:text-white" />
                         </button>
 
                     </div>
--- frontend/src/components/app/share/popup/SharePopup.tsx
+++ frontend/src/components/app/share/popup/SharePopup.tsx
@@ -25,14 +25,14 @@ export default function SharePopup({onSharePress, peopleSharedWith} : SharePopup
     const docName = 'Employment Contract w/ UoA';
 
     return (
-        <div className="drop-shadow-around rounded-lg p-4 flex flex-col gap-2 w-104 bg-white">
-            <h1 className="font-bold">Share '{docName}'</h1>
+        <div className="drop-shadow-around rounded-lg p-4 flex flex-col gap-2 w-104 bg-white dark:bg-anno-space-900 dark:border-2 dark:border-anno-space-100">
+            <h1 className="font-bold text-black dark:text-white">Share '{docName}'</h1>
             <div className="flex flex-row gap-4">
                 <input className="px-2 py-1 grow border-2 border-zinc-300 rounded-lg text-black focus:outline-anno-pink  placeholder:text-neutral-400 placeholder:font-light" placeholder="Enter email address here..." type="email" id="share-email"/>
-                <button type="button" className="bg-anno-red-primary px-4 text-white flex flex-row font-light items-center content-center rounded-lg gap-1 transition-colors hover:bg-anno-red-secondary">Share</button>
+                <button type="button" className="bg-anno-red-primary dark:bg-anno-red-secondary px-4 text-white flex flex-row font-light items-center content-center rounded-lg gap-1 transition-colors hover:bg-anno-red-secondary dark:hover:bg-anno-pink">Share</button>
             </div>
-            <p className="text-neutral-400 text-sm">People with access</p>
-            <div className="overflow-auto flex flex-col border-t-2 border-zinc-300 px-2 py-4  h-60">
+            <p className="text-neutral-400 text-sm dark:text-white">People with access</p>
+            <div className="overflow-auto flex flex-col border-t-2 border-zinc-300 px-2 py-4 h-60">
                 {sharedWithUsers.length
                     ?
                     sharedWithUsers.map((user, index) => (
