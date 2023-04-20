5491024724b65786901cf0d5078b7c3d533372f2
Jia Tee <jiateec@gmail.com>
2023-04-20 12:38:24 +1200

    PDF-54: Active tab styles

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -10,10 +10,9 @@ export default function Modal({isVisible, onOutsideClick, children}: ModalProps)
     if(!isVisible) return null
 
 return ReactDOM.createPortal(
-<div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center">
+<div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center ">
     {isVisible && 
-    
-    <div className="bg-white rounded-lg p-4 z-[200] relative">
+    <div className="bg-white rounded-lg z-[200] relative">
         {children}
         <button onClick={onOutsideClick} className="absolute top-0 right-0 p-2">
             X
--- frontend/src/components/app/setting/SettingModal.tsx
+++ frontend/src/components/app/setting/SettingModal.tsx
@@ -17,27 +17,42 @@ export default function SettingModal({isVisible, onOutsideClick}: SettingModalPr
     const [activeTab, setActiveTab] = useState<SettingTabs>("account")
     const settingModal = useRef<HTMLDivElement>(null);
 
+    const isAccountActive = activeTab === "account" ? "bg-gray-300 dark:bg-anno-space-800" : ""
+    const isSettingsActive = activeTab === "settings" ? "bg-gray-300 dark:bg-anno-space-800" : ""
+
+
     return(
         <Modal isVisible={isVisible} onOutsideClick={onOutsideClick}>
-        <div ref={settingModal} className="grid grid-cols-10 bg-white h-full">
-            <div className="col-span-3 bg-gray-200 h-full">
-                <div className="flex flex-col gap-4">
-                    <span onClick={()=>setActiveTab("account")} className="flex flex-row gap-4 items-center px-4 py-2 transition-colors hover:bg-gray-300 dark:hover:bg-anno-space-800 rounded-lg">
+        <div ref={settingModal} className="grid grid-cols-10 bg-white h-full rounded-md">
+            <div className="col-span-3 bg-gray-200 h-full rounded-md">
+                <div className="flex flex-col gap-4 p-2">
+                    <span onClick={()=>setActiveTab("account")} className={"flex flex-row gap-4 items-center px-4 py-2 transition-colors hover:bg-gray-300 dark:hover:bg-anno-space-800 rounded-lg " + isAccountActive}>
                         <UserCircleIcon className="text-zinc-500 w-6 h-6 dark:text-white"/>
                         <span className="text-zinc-500 dark:text-white">My Account</span>
                     </span>
-                    <span onClick={()=>setActiveTab("settings")} className="flex flex-row gap-4 items-center px-4 py-2 transition-colors hover:bg-gray-300 dark:hover:bg-anno-space-800 rounded-lg">
+                    <span onClick={()=>setActiveTab("settings")} className={"flex flex-row gap-4 items-center px-4 py-2 transition-colors hover:bg-gray-300 dark:hover:bg-anno-space-800 rounded-lg " + isSettingsActive}>
                         <Cog6ToothIcon className="text-zinc-500 w-6 h-6 dark:text-white"/>
                         <span className="text-zinc-500 dark:text-white">Settings</span>
                     </span>
                     </div>
             </div>
-            <div className="col-span-7 p-5">
+            <div className="col-span-7 p-5 rounded-md">
                 {activeTab === "account" && <AccountContent/>}
-                {activeTab === "settings" && <DarkModeToggleTest/>}
+                {activeTab === "settings" && <UserSettings/>}
             </div>
         </div>
         </Modal>
+    )
+}
+
+function UserSettings(){
+    return(
+        <div className="flex flex-col gap-4 h-800">
+            <h1 className="text-2xl font-bold text-anno-red-primary">My Account</h1>
+            <div className="flex flex-col gap-4 ">
+                <DarkModeToggleTest/>
+            </div>
+        </div>
     )
 }
 
