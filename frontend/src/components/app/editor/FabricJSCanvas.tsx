711d57e366387a46a5b99ae6913a0610a5604fe5
Matan Yosef <matan.yosef02@gmail.com>
2023-04-23 09:13:41 +1200

    PDF-65: POC for Math Annotations
    Created by a custom object from fabric js. Still need to look at
    - scaling
    - initialWidth
    - resizing
    - rotation.

--- frontend/src/components/app/editor/FabricJSCanvas.tsx
+++ frontend/src/components/app/editor/FabricJSCanvas.tsx
@@ -1,14 +1,46 @@
-import React, { useEffect, useRef } from 'react';
+import React, {useEffect, useRef} from 'react';
 import { fabric } from 'fabric';
-export default function FabricJSCanvas() {
-    const canvasEl = useRef<HTMLCanvasElement>(null);
-    useEffect(() => {
-        const canvas = new fabric.Canvas('fcanvas');
-        const text = new fabric.Text('Hello, World!', { left: 50, top: 50, selectable: true });
+import CustomObject from "../../../CustomObject";
 
+interface Props {
+    width: number,
+    height: number,
+}
+
+export default function FabricJSCanvas( {width, height} : Props ) {
+    const canvasEl = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        const canvas = new fabric.Canvas(canvasEl.current, {
+            width: width,
+            height:height,
+            backgroundColor: 'white', // TODO REMOVE BG COLOR
+        });
+        const text = new fabric.Textbox('Hello, World!',
+            {
+                left: 50,
+                top: 50,
+                selectable: true,
+                width: 300, // TODO Change initial width,
+            }
+        );
+
+        const customObj = new CustomObject("\\frac{n!}{k!(n-k)!} = \\binom{n}{k}", {
+            left: 100,
+            top: 100,
+            scaleX: 4,
+            scaleY: 4,
+            height: 10,
+            width: 30,
+        });
+
+        // // add your canvas elements here
         canvas.add(text);
-        // add your canvas elements here
+        canvas.add(customObj);
     }, []);
 
-    return <canvas id="fcanvas" />;
+    return (
+        <canvas ref={canvasEl} />
+    );
 };
+
