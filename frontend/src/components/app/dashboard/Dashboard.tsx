e377558a6734ea1c85b8635a1e1619418b4db297
Matan Yosef <matan.yosef02@gmail.com>
2023-04-25 11:08:22 +1200

    PDF-65: Fixed build errors

--- frontend/src/components/app/dashboard/Dashboard.tsx
+++ frontend/src/components/app/dashboard/Dashboard.tsx
@@ -1,14 +1,10 @@
 import DashboardTable from "./DashboardTable";
-import FabricJSCanvas from "../editor/FabricJSCanvas";
-
 
 export default function Dashboard() {
     return (
         <div className="mx-12 flex flex-col gap-4" id="portal-destination">
             <h1 className="text-anno-red-primary text-4xl font-bold">Documents</h1>
             <DashboardTable/>
-            {/* TODO: move this to editor */}
-            <FabricJSCanvas/>
         </div>
 
     );
