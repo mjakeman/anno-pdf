f05090f1f899f9c171232a66bfc29c844ca3a822
Jordan York <yorkjordan07@gmail.com>
2023-04-30 12:43:19 +1200

    Added useState for login form

--- frontend/src/components/public/pages/Login.tsx
+++ frontend/src/components/public/pages/Login.tsx
@@ -1,20 +1,42 @@
 import Container from "../../Container";
 import PrimaryButton from "../../PrimaryButton";
 import googleLogo from "../../../assets/glogo.svg";
-import {useSignInWithGoogle} from "react-firebase-hooks/auth";
+import {useSignInWithEmailAndPassword, useSignInWithGoogle} from "react-firebase-hooks/auth";
 import {auth} from "../../../firebaseAuth";
+import {ChangeEvent, useState} from "react";
 
 export default function Login() {
 
     // TODO: Add signin with google (similar to SignUp.tsx)
     // TODO: Add signin with email and password https://github.com/CSFrequency/react-firebase-hooks/tree/master/auth#usesigninwithemailandpassword
 
+    const [loginForm, setLoginForm] = useState({
+       email: "",
+       password: ""
+    });
+
     const [signInWithGoogle] = useSignInWithGoogle(auth)
+    const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth)
+
+    const handleLoginFormChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setLoginForm({
+            ...loginForm,
+            [event.target.name]: event.target.value,
+        });
+    };
 
     async function handleSignInWithGoogle() {
         try {
             const signedInUser = await signInWithGoogle();
             // TODO: Add API call here to check for google authentication
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function handleSignInWithEmailandPass() {
+        try {
+            const signedInUser = await signInWithEmailAndPassword(loginForm.email, loginForm.password);
         } catch (error) {
             console.error(error);
         }
@@ -27,14 +49,14 @@ export default function Login() {
                 <form className="my-8 px-24 py-12 flex flex-col gap-6 bg-white dark:bg-anno-space-900 drop-shadow-around border-2 dark:border-anno-space-100 rounded-xl">
                     <div className="w-96">
                         <label className="mb-2 text-neutral-400 dark:text-white" htmlFor="email-input" >Email:</label>
-                        <input type="email" id="email-input" placeholder="Enter your email address here..." className="bg-white dark:bg-anno-space-700 px-2 py-1 border-2 border-zinc-300 rounded-lg placeholder:text-neutral-400 placeholder:font-light focus:outline-none focus:border-blue-500 w-full rounded-md focus:ring-1 dark:focus:invalid:bg-pink-200 dark:text-white invalid:text-pink-500 focus:invalid:text-pink-500 invalid:border-pink-600 invalid:ring-pink-500 focus:invalid:border-pink-600 focus:invalid:ring-pink-500"/>
+                        <input onChange={handleLoginFormChange} value={loginForm.email} type="email" id="email-input" placeholder="Enter your email address here..." className="bg-white dark:bg-anno-space-700 px-2 py-1 border-2 border-zinc-300 rounded-lg placeholder:text-neutral-400 placeholder:font-light focus:outline-none focus:border-blue-500 w-full rounded-md focus:ring-1 dark:focus:invalid:bg-pink-200 dark:text-white invalid:text-pink-500 focus:invalid:text-pink-500 invalid:border-pink-600 invalid:ring-pink-500 focus:invalid:border-pink-600 focus:invalid:ring-pink-500"/>
                     </div>
                     <div className="w-96">
                         <label className="mb-2 text-neutral-400 dark:text-white" htmlFor="password-input" >Password:</label>
-                        <input type="password" id="password-input" placeholder="Enter your password here..." className="bg-white dark:bg-anno-space-700 px-2 py-1 border-2 border-zinc-300 rounded-lg placeholder:text-neutral-400 placeholder:font-light focus:outline-none focus:border-blue-500 w-full rounded-md focus:ring-1 dark:focus:invalid:bg-pink-200 dark:text-white invalid:text-pink-500 focus:invalid:text-pink-500 invalid:border-pink-600 invalid:ring-pink-500 focus:invalid:border-pink-600 focus:invalid:ring-pink-500"/>
+                        <input onChange={handleLoginFormChange} value={loginForm.password} type="password" id="password-input" placeholder="Enter your password here..." className="bg-white dark:bg-anno-space-700 px-2 py-1 border-2 border-zinc-300 rounded-lg placeholder:text-neutral-400 placeholder:font-light focus:outline-none focus:border-blue-500 w-full rounded-md focus:ring-1 dark:focus:invalid:bg-pink-200 dark:text-white invalid:text-pink-500 focus:invalid:text-pink-500 invalid:border-pink-600 invalid:ring-pink-500 focus:invalid:border-pink-600 focus:invalid:ring-pink-500"/>
                     </div>
 
-                   <PrimaryButton onClick={handleSignInWithGoogle} label="Log in"/>
+                   <PrimaryButton onClick={handleSignInWithEmailandPass} label="Log in"/>
 
                     <div className="flex flex-row gap-6 items-center text-zinc-500 dark:text-white">
                         <hr className="w-full" />
