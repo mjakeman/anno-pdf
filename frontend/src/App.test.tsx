f96ac1d326e2d99aa7249f7b21e2a2211f6448ef
Jia Tee <jiateec@gmail.com>
2023-04-30 14:10:59 +1200

    PDF-39: Vitest setup, react testing lib/jest dom

---
+++ frontend/src/App.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import { describe, it, expect } from 'vitest'
+import DashboardTable from './components/app/dashboard/DashboardTable';
+import '@testing-library/jest-dom'
+import About from './components/public/pages/About';
+
+describe('App', () => {
+    it('Jest equivalent test', () => {
+        console.log("Test");
+        expect(true).toBe(true);
+    });
+
+    it('About page renders', () => {
+        render(<About />);
+        expect(screen.getByText('About Us')).toBeInTheDocument();
+    });
+
+});
+
+
