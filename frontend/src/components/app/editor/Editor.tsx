f4262595812900fc9177cf2e214571d830eb375d
Matan Yosef <matan.yosef02@gmail.com>
2023-05-02 16:50:41 +1200

    PDF-81: Removed Unused Components from layout

--- frontend/src/components/app/editor/Editor.tsx
+++ frontend/src/components/app/editor/Editor.tsx
@@ -1,7 +1,5 @@
-import {ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/outline";
 import Toolbar from "./toolbar/Toolbar";
-import Viewer from "./Viewer";
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import EditorHeader from "./header/EditorHeader";
 import Pan from "./toolbar/model/tools/Pan";
 import Tool from "./toolbar/model/tools/Tool";
@@ -29,24 +27,8 @@ export default function Editor() {
                 <main className="grow bg-zinc-300 dark:bg-anno-space-700">
 
                     <DocumentViewer documentUuid={"test"}/>
-                    {/* TODO: Make react component */}
-                    <div className="absolute top-[50%] left-0 ml-4">
-                        <button className="transition-all bg-black opacity-50 hover:opacity-70 rounded-full p-2" onClick={e => setPageNumber(pageNumber - 1)}>
-                            <ArrowLeftIcon className="text-white h-6 w-6"/>
-                        </button>
-                    </div>
-
-                    <div className="absolute top-[50%] right-0 mr-4">
-                        <button className="transition-all bg-black opacity-50 hover:opacity-70 rounded-full p-2" onClick={e => setPageNumber(pageNumber + 1)}>
-                            <ArrowRightIcon className="text-white h-6 w-6"/>
-                        </button>
-                    </div>
 
                 </main>
-
-                {/*<div className="justify-self-end">*/}
-                {/*    <EditorFooter />*/}
-                {/*</div>*/}
             </div>
         </ToolContext.Provider>
     );
