605b4f6caf781ec7e53c4a7a4440ea74a923aead
Jordan York <yorkjordan07@gmail.com>
2023-05-06 14:01:49 +1200

    PDF-64 - Basic login functionality complete

--- frontend/src/components/public/pages/Login.tsx
+++ frontend/src/components/public/pages/Login.tsx
@@ -18,63 +18,63 @@ export default function Login() {
 
     const navigate = useNavigate();
 
+    const errorMessage = 'Error whilst logging in. Please try again';
+
     async function handleSignInWithGoogle() {
-        try {
-            await signInWithGoogle();
 
-            if (googleUser) {
-                var loginJsonData = {
-                    "uid" : googleUser.user.uid,
-                    "name" : googleUser.user.displayName?.replaceAll(" ", ""),
-                    "email" : googleUser.user.email,
-                }
+        await signInWithGoogle();
 
-                let token = await googleUser?.user.getIdToken();
-                axios.post('http://localhost:8080/user',loginJsonData, {
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                }).then(function (response) {
-                    console.log(response);
-                })
-                    .catch(function (error) {
-                        setError(error);
-                    });
-
-                navigate("/project-group-fearless-foxes/dash");
+        if (googleUser) {
+            var loginJsonData = {
+                "uid": googleUser.user.uid,
+                "name": googleUser.user.displayName?.replaceAll(" ", ""),
+                "email": googleUser.user.email,
             }
-        } catch (error) {
-            setError('Error whilst logging in. Please check Google account is correct and try again');
+
+            let token = await googleUser?.user.getIdToken();
+            axios.post('http://localhost:8080/user', loginJsonData, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then(function (response) {
+            }).catch(function (error) {
+                if (error.response.status == 422) {
+                    navigate("/dash");
+                } else {
+                    setError(error);
+                }
+            });
+        } else {
+            setError(errorMessage);
         }
     }
 
     async function handleSignInWithEmailandPassword() {
-        try {
-            await signInWithEmailAndPassword(email, password);
 
-            if (user) {
-                var loginJsonData = {
-                    "uid" : user.user.uid,
-                    "name" : user.user.displayName?.replaceAll(" ", ""),
-                    "email" : user.user.email,
-                }
+        await signInWithEmailAndPassword(email, password);
 
-                let token = await user?.user.getIdToken();
-                axios.post('http://localhost:8080/user',loginJsonData, {
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                }).then(function (response) {
-                    console.log(response);
-                })
-                    .catch(function (error) {
-                        setError(error);
-                    });
-                navigate("/project-group-fearless-foxes/dash");
+        if (user) {
+            var loginJsonData = {
+                "uid": user.user.uid,
+                "name": user.user.displayName?.replaceAll(" ", ""),
+                "email": user.user.email,
             }
 
-        } catch (error) {
-            setError('Error whilst logging in. Please check email and password are correct and try again');
+            let token = await user?.user.getIdToken();
+            axios.post('http://localhost:8080/user', loginJsonData, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then(function (response) {
+            }).catch(function (error) {
+                if (error.response.status == 422) {
+                    navigate('/dash');
+                } else {
+                    setError(errorMessage);
+                }
+            });
+        } else {
+            setError(errorMessage);
         }
     }
 
@@ -82,35 +82,44 @@ export default function Login() {
         <Container>
             <div className="flex flex-col items-center my-12">
                 <h1 className="text-4xl font-bold text-anno-red-primary dark:text-anno-pink-500 text-center">Log in</h1>
-                <form className="my-8 px-24 py-12 flex flex-col gap-6 bg-white dark:bg-anno-space-900 drop-shadow-around border-2 dark:border-anno-space-100 rounded-xl">
+                <form
+                    className="my-8 px-24 py-12 flex flex-col gap-6 bg-white dark:bg-anno-space-900 drop-shadow-around border-2 dark:border-anno-space-100 rounded-xl">
                     <div className="w-96">
-                        <label className="mb-2 text-neutral-400 dark:text-white" htmlFor="email-input" >Email:</label>
-                        <input onChange={e => setEmail(e.target.value)} value={email} type="email" id="email-input" placeholder="Enter your email address here..." className="bg-white dark:bg-anno-space-700 px-2 py-1 border-2 border-zinc-300 rounded-lg placeholder:text-neutral-400 placeholder:font-light focus:outline-none focus:border-blue-500 w-full rounded-md focus:ring-1 dark:focus:invalid:bg-pink-200 dark:text-white invalid:text-pink-500 focus:invalid:text-pink-500 invalid:border-pink-600 invalid:ring-pink-500 focus:invalid:border-pink-600 focus:invalid:ring-pink-500"/>
+                        <label className="mb-2 text-neutral-400 dark:text-white" htmlFor="email-input">Email:</label>
+                        <input onChange={e => setEmail(e.target.value)} value={email} type="email" id="email-input"
+                               placeholder="Enter your email address here..."
+                               className="bg-white dark:bg-anno-space-700 px-2 py-1 border-2 border-zinc-300 rounded-lg placeholder:text-neutral-400 placeholder:font-light focus:outline-none focus:border-blue-500 w-full rounded-md focus:ring-1 dark:focus:invalid:bg-pink-200 dark:text-white invalid:text-pink-500 focus:invalid:text-pink-500 invalid:border-pink-600 invalid:ring-pink-500 focus:invalid:border-pink-600 focus:invalid:ring-pink-500"/>
                     </div>
                     <div className="w-96">
-                        <label className="mb-2 text-neutral-400 dark:text-white" htmlFor="password-input" >Password:</label>
-                        <input onChange={e => setPassword(e.target.value)} value={password} type="password" id="password-input" placeholder="Enter your password here..." className="bg-white dark:bg-anno-space-700 px-2 py-1 border-2 border-zinc-300 rounded-lg placeholder:text-neutral-400 placeholder:font-light focus:outline-none focus:border-blue-500 w-full rounded-md focus:ring-1 dark:focus:invalid:bg-pink-200 dark:text-white invalid:text-pink-500 focus:invalid:text-pink-500 invalid:border-pink-600 invalid:ring-pink-500 focus:invalid:border-pink-600 focus:invalid:ring-pink-500"/>
+                        <label className="mb-2 text-neutral-400 dark:text-white"
+                               htmlFor="password-input">Password:</label>
+                        <input onChange={e => setPassword(e.target.value)} value={password} type="password"
+                               id="password-input" placeholder="Enter your password here..."
+                               className="bg-white dark:bg-anno-space-700 px-2 py-1 border-2 border-zinc-300 rounded-lg placeholder:text-neutral-400 placeholder:font-light focus:outline-none focus:border-blue-500 w-full rounded-md focus:ring-1 dark:focus:invalid:bg-pink-200 dark:text-white invalid:text-pink-500 focus:invalid:text-pink-500 invalid:border-pink-600 invalid:ring-pink-500 focus:invalid:border-pink-600 focus:invalid:ring-pink-500"/>
                     </div>
 
-                   <PrimaryButton onClick={handleSignInWithEmailandPassword} label="Log in"/>
+                    <PrimaryButton onClick={handleSignInWithEmailandPassword} label="Log in"/>
 
-                    {error && <div className="bg-anno-red-secondary bg-opacity-70 py-3 px-4 text-white flex flex-row items-center justify-center rounded-lg gap-1 text-sm transition-colors">
+                    {error !== '' && <div
+                        className="bg-anno-red-secondary bg-opacity-70 py-3 px-4 text-white flex flex-row items-center justify-center gap-1 text-sm">
                         {error}
                     </div>}
 
                     <div className="flex flex-row gap-6 items-center text-zinc-500 dark:text-white">
-                        <hr className="w-full" />
+                        <hr className="w-full"/>
                         <span>or</span>
-                        <hr className="w-full" />
+                        <hr className="w-full"/>
                     </div>
 
-                    {/* Continue with Google Button - TODO: add functionality or replace with package */}
-                    <button type="button" onClick={handleSignInWithGoogle} className="flex flex-row items-center justify-center gap-4 bg-white border-2 p-2 rounded transition-colors hover:bg-zinc-200">
-                        <img className="w-6 h-6" src={googleLogo} alt="Google logo" />
+                    <button type="button" onClick={handleSignInWithGoogle}
+                            className="flex flex-row items-center justify-center gap-4 bg-white border-2 p-2 rounded transition-colors hover:bg-zinc-200">
+                        <img className="w-6 h-6" src={googleLogo} alt="Google logo"/>
                         <span className="text-lg font-light">Continue with Google</span>
                     </button>
 
-                    <a href="#" className="text-zinc-400 underline text-center transition-colors hover:text-anno-pink-500 dark:hover:text-anno-pink-500 dark:text-white ">Forgot password?</a>
+                    <a href="#"
+                       className="text-zinc-400 underline text-center transition-colors hover:text-anno-pink-500 dark:hover:text-anno-pink-500 dark:text-white ">Forgot
+                        password?</a>
 
                 </form>
             </div>
--- frontend/src/components/public/pages/SignUp.tsx
+++ frontend/src/components/public/pages/SignUp.tsx
@@ -2,11 +2,11 @@ import Container from "../../Container";
 import PrimaryButton from "../../PrimaryButton";
 import googleLogo from "../../../assets/glogo.svg";
 import {auth} from "../../../firebaseAuth";
-import { useSignInWithGoogle, useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import {useSignInWithGoogle, useCreateUserWithEmailAndPassword} from 'react-firebase-hooks/auth';
 import {ChangeEvent, useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import axios from "axios";
-import { updateProfile } from "firebase/auth";
+import {updateProfile} from "firebase/auth";
 
 export default function SignUp() {
 
@@ -21,6 +21,8 @@ export default function SignUp() {
     });
 
     const [error, setError] = useState('');
+
+    const errorMessage = 'Error whilst signing up. Please try again';
 
     const navigate = useNavigate();
 
@@ -39,111 +41,127 @@ export default function SignUp() {
     };
 
     async function handleSignUpWithGoogle() {
-        try {
-            await signInWithGoogle();
+        await signInWithGoogle();
 
-            if (googleUser) {
+        if (googleUser) {
 
-                var loginJsonData = {
-                    "uid" : googleUser.user.uid,
-                    "name" : googleUser.user.displayName?.replaceAll(" ", ""),
-                    "email" : googleUser.user.email,
-                }
-
-                let token = await googleUser?.user.getIdToken();
-                axios.post('http://localhost:8080/user',loginJsonData, {
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                }).then(function (response) {
-                    console.log(response);
-                })
-                    .catch(function (error) {
-                        setError(error);
-                    });
-
-                navigate("/project-group-fearless-foxes/dash");
+            var loginJsonData = {
+                "uid": googleUser.user.uid,
+                "name": googleUser.user.displayName?.replaceAll(" ", ""),
+                "email": googleUser.user.email,
             }
-        } catch (error) {
-            setError('Error whilst logging in. Please check Google Account is correct and try again');
+
+            let token = await googleUser?.user.getIdToken();
+            axios.post('http://localhost:8080/user', loginJsonData, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then(function (response) {
+                if (response.status == 200) {
+                    navigate("/dash");
+                }
+            }).catch(function (error) {
+                if (error.response.status == 422) {
+                    setError('User already exists. Please login instead.');
+                } else {
+                    setError(errorMessage);
+                }
+            });
+        } else {
+            setError(errorMessage);
         }
     }
 
     async function handleDefaultSignUpSubmit(event: MouseEvent) {
-        try {
-            await createUserWithEmailAndPassword(signUpForm.email, signUpForm.password);
 
-            if (user) {
+        await createUserWithEmailAndPassword(signUpForm.email, signUpForm.password);
 
-                var loginJsonData = {
-                    "uid" : user.user.uid,
-                    "name" : signUpForm.firstName + signUpForm.lastName,
-                    "email" : signUpForm.email,
-                }
+        if (user) {
 
-                let token = await googleUser?.user.getIdToken();
-                axios.post('http://localhost:8080/user',loginJsonData, {
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                }).then(function (response) {
-                    console.log(response);
-                })
-                    .catch(function (error) {
-                        setError(error);
-                    });
-
-                navigate("/project-group-fearless-foxes/dash");
+            var loginJsonData = {
+                "uid": user.user.uid,
+                "name": signUpForm.firstName + signUpForm.lastName,
+                "email": signUpForm.email,
             }
-            console.log(user);
-            if(user){
-                await updateProfile(user.user, {
-                    displayName: signUpForm.firstName+signUpForm.lastName
-                })
-            }    
-        } catch (error) {
-            setError('Error whilst logging in. Please check email and password are correct and try again');
+
+            let token = await googleUser?.user.getIdToken();
+            axios.post('http://localhost:8080/user', loginJsonData, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then(function (response) {
+                if (response.status == 200) {
+                    navigate("/dash");
+                }
+            }).catch(function (error) {
+                if (error.code == 422) {
+                    setError('User already exists. Please login instead.')
+                } else {
+                    setError(errorMessage);
+                }
+            });
+
+            await updateProfile(user.user, {
+                displayName: signUpForm.firstName + signUpForm.lastName
+            });
+        } else {
+            setError(errorMessage);
         }
     }
 
     return (
         <Container>
             <div className="flex flex-col items-center my-12">
-                <h1 className="text-4xl font-bold text-anno-red-primary dark:text-anno-pink-500 text-center">Sign up</h1>
-                <form className="my-8 px-24 py-12 flex flex-col gap-6 bg-white dark:bg-anno-space-900 drop-shadow-around border-2 dark:border-anno-space-100 rounded-xl">
+                <h1 className="text-4xl font-bold text-anno-red-primary dark:text-anno-pink-500 text-center">Sign
+                    up</h1>
+                <form
+                    className="my-8 px-24 py-12 flex flex-col gap-6 bg-white dark:bg-anno-space-900 drop-shadow-around border-2 dark:border-anno-space-100 rounded-xl">
                     <div className="w-96 flex flex-row gap-4">
                         <span>
-                            <label className="mb-2 text-neutral-400 dark:text-white" htmlFor="fname-input" >First Name:</label>
-                            <input onChange={handleSignUpFormChange} value={signUpForm.firstName} type="text" id="fname-input" name="firstName" placeholder="Enter your first name here..." className="bg-white dark:bg-anno-space-700 px-2 py-1 border-2 border-zinc-300 rounded-lg placeholder:text-neutral-400 placeholder:font-light focus:outline-none focus:border-blue-500 w-full rounded-md focus:ring-1 dark:focus:invalid:bg-pink-200 dark:text-white focus:invalid:border-pink-600 focus:invalid:ring-pink-500"/>
+                            <label className="mb-2 text-neutral-400 dark:text-white"
+                                   htmlFor="fname-input">First Name:</label>
+                            <input onChange={handleSignUpFormChange} value={signUpForm.firstName} type="text"
+                                   id="fname-input" name="firstName" placeholder="Enter your first name here..."
+                                   className="bg-white dark:bg-anno-space-700 px-2 py-1 border-2 border-zinc-300 rounded-lg placeholder:text-neutral-400 placeholder:font-light focus:outline-none focus:border-blue-500 w-full rounded-md focus:ring-1 dark:focus:invalid:bg-pink-200 dark:text-white focus:invalid:border-pink-600 focus:invalid:ring-pink-500"/>
                         </span>
                         <span>
-                            <label className="mb-2 text-neutral-400 dark:text-white" htmlFor="jname-input" >Last Name:</label>
-                            <input onChange={handleSignUpFormChange} value={signUpForm.lastName} type="text" id="lname-input" name="lastName" placeholder="Enter your last name here..." className="bg-white dark:bg-anno-space-700 px-2 py-1 border-2 border-zinc-300 rounded-lg placeholder:text-neutral-400 placeholder:font-light focus:outline-none focus:border-blue-500 w-full rounded-md focus:ring-1 dark:focus:invalid:bg-pink-200 dark:text-white focus:invalid:border-pink-600 focus:invalid:ring-pink-500"/>
+                            <label className="mb-2 text-neutral-400 dark:text-white"
+                                   htmlFor="jname-input">Last Name:</label>
+                            <input onChange={handleSignUpFormChange} value={signUpForm.lastName} type="text"
+                                   id="lname-input" name="lastName" placeholder="Enter your last name here..."
+                                   className="bg-white dark:bg-anno-space-700 px-2 py-1 border-2 border-zinc-300 rounded-lg placeholder:text-neutral-400 placeholder:font-light focus:outline-none focus:border-blue-500 w-full rounded-md focus:ring-1 dark:focus:invalid:bg-pink-200 dark:text-white focus:invalid:border-pink-600 focus:invalid:ring-pink-500"/>
                         </span>
                     </div>
                     <div className="w-96">
-                        <label className="mb-2 text-neutral-400 dark:text-white" htmlFor="email-input" >Email:</label>
-                        <input onChange={handleSignUpFormChange} value={signUpForm.email} type="email" id="email-input" name="email" placeholder="Enter your email address here..." className="bg-white dark:bg-anno-space-700 px-2 py-1 border-2 border-zinc-300 rounded-lg placeholder:text-neutral-400 placeholder:font-light focus:outline-none focus:border-blue-500 w-full rounded-md focus:ring-1 dark:focus:invalid:bg-pink-200 dark:text-white invalid:text-pink-500 focus:invalid:text-pink-500 invalid:border-pink-600 invalid:ring-pink-500 focus:invalid:border-pink-600 focus:invalid:ring-pink-500"/>
+                        <label className="mb-2 text-neutral-400 dark:text-white" htmlFor="email-input">Email:</label>
+                        <input onChange={handleSignUpFormChange} value={signUpForm.email} type="email" id="email-input"
+                               name="email" placeholder="Enter your email address here..."
+                               className="bg-white dark:bg-anno-space-700 px-2 py-1 border-2 border-zinc-300 rounded-lg placeholder:text-neutral-400 placeholder:font-light focus:outline-none focus:border-blue-500 w-full rounded-md focus:ring-1 dark:focus:invalid:bg-pink-200 dark:text-white invalid:text-pink-500 focus:invalid:text-pink-500 invalid:border-pink-600 invalid:ring-pink-500 focus:invalid:border-pink-600 focus:invalid:ring-pink-500"/>
                     </div>
                     <div className="w-96">
-                        <label className="mb-2 text-neutral-400 dark:text-white" htmlFor="password-input" >Password:</label>
-                        <input onChange={handleSignUpFormChange} value={signUpForm.password} type="password" id="password-input" name="password" placeholder="Enter your password here..." className="bg-white dark:bg-anno-space-700 px-2 py-1 border-2 border-zinc-300 rounded-lg placeholder:text-neutral-400 placeholder:font-light focus:outline-none focus:border-blue-500 w-full rounded-md focus:ring-1 dark:focus:invalid:bg-pink-200 dark:text-white invalid:text-pink-500 focus:invalid:text-pink-500 invalid:border-pink-600 invalid:ring-pink-500 focus:invalid:border-pink-600 focus:invalid:ring-pink-500"/>
+                        <label className="mb-2 text-neutral-400 dark:text-white"
+                               htmlFor="password-input">Password:</label>
+                        <input onChange={handleSignUpFormChange} value={signUpForm.password} type="password"
+                               id="password-input" name="password" placeholder="Enter your password here..."
+                               className="bg-white dark:bg-anno-space-700 px-2 py-1 border-2 border-zinc-300 rounded-lg placeholder:text-neutral-400 placeholder:font-light focus:outline-none focus:border-blue-500 w-full rounded-md focus:ring-1 dark:focus:invalid:bg-pink-200 dark:text-white invalid:text-pink-500 focus:invalid:text-pink-500 invalid:border-pink-600 invalid:ring-pink-500 focus:invalid:border-pink-600 focus:invalid:ring-pink-500"/>
                     </div>
 
                     <PrimaryButton onClick={handleDefaultSignUpSubmit} label="Sign up"/>
 
-                    {error && <div className="bg-anno-red-secondary bg-opacity-70 py-3 px-4 text-white flex flex-row items-center justify-center gap-1 text-sm transition-colors">
+                    {error && <div
+                        className="bg-anno-red-secondary bg-opacity-70 py-3 px-4 text-white flex flex-row items-center justify-center gap-1 text-sm">
                         {error}
                     </div>}
 
                     <div className="flex flex-row gap-6 items-center text-zinc-500 dark:text-white">
-                        <hr className="w-full" />
+                        <hr className="w-full"/>
                         <span>or</span>
-                        <hr className="w-full" />
+                        <hr className="w-full"/>
                     </div>
 
-                    <button onClick={()=>handleSignUpWithGoogle()} type="button" className="flex flex-row items-center justify-center gap-4 bg-white border-2 p-2 rounded transition-colors hover:bg-zinc-200">
-                        <img className="w-6 h-6" src={googleLogo} alt="Google logo" />
+                    <button onClick={() => handleSignUpWithGoogle()} type="button"
+                            className="flex flex-row items-center justify-center gap-4 bg-white border-2 p-2 rounded transition-colors hover:bg-zinc-200">
+                        <img className="w-6 h-6" src={googleLogo} alt="Google logo"/>
                         <span className="text-lg font-light">Continue with Google</span>
                     </button>
 
