9160e7a2b1553b55207d72d2e94d9d3c07d3313d
Matan Yosef <matan.yosef02@gmail.com>
2023-05-09 18:16:17 +1200

    PDF-127: Connected share / remove
    Co-Authored-By: JJTee <73531134+JJTee@users.noreply.github.com>

--- frontend/src/components/app/share/popup/SharePopup.tsx
+++ frontend/src/components/app/share/popup/SharePopup.tsx
@@ -1,39 +1,63 @@
-import React, {useRef, useState} from "react";
+import React, {useContext, useRef, useState} from "react";
 import SharedWithUserRow from "./SharedWithUserRow";
 import useDetectOutsideClick from "../../../../hooks/useDetectOutsideClick";
-import {Owner, SharedUser} from "../../editor/Models";
+import {AnnoDocument, Owner, SharedUser} from "../../editor/Models";
+import {AuthContext} from "../../../../contexts/AuthContextProvider";
+import axios from "axios";
+import {useToast} from "../../../../hooks/useToast";
 
 interface SharePopupProps {
     owner: Owner,
-    docName: string,
+    annoDocument: AnnoDocument,
     onSharePress:  (params: any) => any;
 
     peopleSharedWith: SharedUser[],
     onOutsideClick: (params: any) => any;
 }
-export default function SharePopup({owner, docName, onSharePress, peopleSharedWith, onOutsideClick } : SharePopupProps) {
+export default function SharePopup({owner, annoDocument, onSharePress, peopleSharedWith, onOutsideClick } : SharePopupProps) {
 
+    const {firebaseUserRef} = useContext(AuthContext);
     const shareDropdown = useRef<HTMLDivElement>(null);
     const [email, setEmail] = useState('');
+    const {addToast} = useToast();
 
     useDetectOutsideClick(shareDropdown, onOutsideClick);
 
     const [sharedWithUsers, setSharedWithUsers] = useState<SharedUser[]>(peopleSharedWith);
 
-    function removeFromPeople(emailOfPersonToBeRemoved: string, peopleList: SharedUser[]) {
-        const indexOfObject = peopleList.findIndex(object => {
-            return object.email === emailOfPersonToBeRemoved;
+    async function removeFromPeople(emailOfPersonToBeRemoved: string, peopleList: SharedUser[]) {
+
+        let token = await firebaseUserRef!.getIdToken();
+        axios.post(`${import.meta.env.VITE_BACKEND_URL}/documents/${annoDocument.uuid}/removeUser`, {email: emailOfPersonToBeRemoved} , {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then(() => {
+            const indexOfObject = peopleList.findIndex(object => {
+                return object.email === emailOfPersonToBeRemoved;
+            });
+            // TODO: This is where we query!
+            setSharedWithUsers([
+                ...peopleList.slice(0, indexOfObject),
+                ...peopleList.slice(indexOfObject + 1),
+            ]);
+            addToast({
+                position: 'bottom-right',
+                message: 'Successfully removed user.',
+                type: 'success'
+            })
+        }).catch(error => {
+            addToast({
+                position: 'bottom-right',
+                message: 'Failed to remove user. Please double check you are the document owner and try again.',
+                type: 'error'
+            })
         });
-        // TODO: This is where we query!
-        setSharedWithUsers([
-            ...peopleList.slice(0, indexOfObject),
-            ...peopleList.slice(indexOfObject + 1),
-        ]);
     }
 
     return (
         <div ref={shareDropdown} className="drop-shadow-around rounded-lg p-4 flex flex-col gap-2 w-104 bg-white dark:bg-anno-space-900 dark:border-2 dark:border-anno-space-100">
-            <h1 className="font-bold text-black dark:text-white">Share '{docName}'</h1>
+            <h1 className="font-bold text-black dark:text-white">Share '{annoDocument.title}'</h1>
             <div className="flex flex-row gap-4">
                 <input value={email} onChange={e => setEmail(e.target.value)} className="px-2 py-1 grow border-2 border-zinc-300 rounded-lg text-black focus:outline-anno-pink-500 placeholder:text-neutral-400 placeholder:font-light" placeholder="Enter email address here..." type="email" id="share-email"/>
                 <button onClick={()=>onSharePress(email)}type="button" className="bg-anno-red-primary dark:bg-anno-red-secondary px-4 text-white flex flex-row font-light items-center content-center rounded-lg gap-1 transition-colors hover:bg-anno-red-secondary dark:hover:bg-anno-pink-500">Share</button>
