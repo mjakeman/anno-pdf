f038e4dc0ed385f4378f85cfc53d7435e065537f
Matan Yosef <matan.yosef02@gmail.com>
2023-05-09 16:22:21 +1200

    PDF-127: Refactored to use document object
    This is a fairly big commit, but it will make loading smoother, and will mean we can get all the data we need much smoother.

--- frontend/src/components/app/editor/Editor.tsx
+++ frontend/src/components/app/editor/Editor.tsx
@@ -1,10 +1,15 @@
 import Toolbar from "./toolbar/Toolbar";
-import React, {useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import EditorHeader from "./header/EditorHeader";
 import Pan from "./toolbar/model/tools/Pan";
 import Tool from "./toolbar/model/tools/Tool";
 import DocumentViewer from "./DocumentViewer";
 import {useParams} from "react-router-dom";
+import {AuthContext} from "../../../contexts/AuthContextProvider";
+import {AnnoDocument} from "./Models";
+import axios from "axios";
+import AnimatedSpinner from "../AnimatedSpinner";
+import {useToast} from "../../../hooks/useToast";
 export const ToolContext = React.createContext<any[]>([]);
 export const ZoomContext = React.createContext<any[]>([]);
 
@@ -13,11 +18,44 @@ export default function Editor() {
     const [activeToolData, setActiveToolData] = useState<Tool>(new Pan("pan"));
     const [zoom, setZoom] = useState(100); // Initial Zoom
     let  { documentUuid } = useParams();
+    const {currentUser, firebaseUserRef} = useContext(AuthContext);
+    const [document, setDocument] = useState<AnnoDocument | null>(null);
+
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        if (!firebaseUserRef) return;
+        firebaseUserRef!.getIdToken()
+            .then((token) => {
+                axios.get(`${import.meta.env.VITE_BACKEND_URL}/documents/${documentUuid}/`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }).then(function (response) {
+                    setDocument({
+                        uuid: response.data.uuid,
+                        title: response.data.title,
+                        createdAt: response.data.createdAt,
+                        updatedAt: response.data.updatedAt,
+                        base64File: response.data.base64file,
+                        sharedWith: response.data.sharedWith, // Array of Users
+                        owner: response.data.owner,
+                        annotations: response.data.annotations,
+                    });
+                }).catch(function (error) {
+                    console.log(error);
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, [currentUser]);
 
 
     useEffect(() => {
         setActiveToolData(new Pan("pan"));
     }, []);
+
 
     return (
         <ToolContext.Provider value={[activeToolData, setActiveToolData]}>
@@ -32,9 +70,20 @@ export default function Editor() {
                     </div>
 
                     {/* Document Space */}
-                    <main className="grow bg-zinc-300 dark:bg-anno-space-700 overflow-y-hidden">
+                    <main className="grow bg-zinc-300 dark:bg-anno-space-700 overflow-y-auto">
 
-                        <DocumentViewer documentUuid={documentUuid}/>
+                        {!isLoaded &&
+                                <div className="grid place-items-center h-full ">
+                                <AnimatedSpinner className={"h-36 w-36 text-white"}/>
+                            </div>
+                        }
+                        {document &&
+                            <>
+                                <span className={`${isLoaded ? "block" : "hidden"} `}>
+                                    <DocumentViewer onDocumentLoaded={() => setIsLoaded(true)} document={document} />
+                                </span>
+                            </>
+                        }
 
                     </main>
                 </div>
---
+++ frontend/src/components/app/editor/Models.ts
@@ -0,0 +1,24 @@
+import {fabric} from "fabric";
+
+export interface SharedUser {
+    uuid: string | null; // Null if the user doesn't exist yet (i.e. sharing with someone who doesn't have an Anno account)
+    name: string | null; // Null if the user doesn't exist yet
+    email: string;
+}
+
+export interface Owner {
+    uuid: string;
+    name: string;
+    email: string;
+}
+
+export interface AnnoDocument {
+    uuid: string,
+    title: string,
+    createdAt: string,
+    updatedAt: string,
+    base64File: string,
+    sharedWith: SharedUser[] // Array of Users
+    owner: Owner
+    annotations: fabric.Canvas[]
+}
