1a726f156021124503fa521ae7b69168c9e27461
Jia Tee <jiateec@gmail.com>
2023-05-10 18:09:15 +1200

    PDF-133 - update user bubbles to email

--- frontend/src/components/app/editor/header/ActiveUserBubble.tsx
+++ frontend/src/components/app/editor/header/ActiveUserBubble.tsx
@@ -12,12 +12,12 @@ export default function ActiveUserBubble({user} : ActiveUserBubbleProps) {
     return (
         <div className="relative">
             <span onMouseOver={() => setShowUserInfoCard(true)} onMouseLeave={() => setShowUserInfoCard(false)} className="hover:cursor-pointer">
-                <ProfileBubble size={ProfileBubbleSizes.Large} name={user.fullName}></ProfileBubble>
+                <ProfileBubble size={ProfileBubbleSizes.Large} email={user.email}></ProfileBubble>
             </span>
 
             {/* Shown only on hover... */}
             <div className={`${!showUserInfoCard && "hidden"} right-0 absolute z-50 bg-white dark:bg-anno-space-900 dark:border-2 dark:border-anno-space-100 shadow px-6 py-4 rounded-xl flex flex-row items-center gap-2`}>
-                <ProfileBubble size={ProfileBubbleSizes.Small} name={user.fullName}/>
+                <ProfileBubble size={ProfileBubbleSizes.Small} email={user.email}/>
                 <div className="flex flex-col grow">
                     <p className="font-medium text-black dark:text-white">{user.fullName}</p>
                     <span className="text-slate-500 text-sm dark:text-stone-300">{user.email}</span>
