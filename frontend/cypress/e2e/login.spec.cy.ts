3437551aadc1a34daf643db62f55e6818dd04518
Jia Tee <jiateec@gmail.com>
2023-05-10 23:14:35 +1200

    PDF-146 use contains

--- frontend/cypress/e2e/login.spec.cy.ts
+++ frontend/cypress/e2e/login.spec.cy.ts
@@ -1,11 +1,14 @@
 describe('Login functionality', () => {
   it('Error logging in with wrong credentials', () => {
     cy.visit('http://localhost:5173/login')
+    cy.log('Login page loaded and no error message is visible');
     cy.get('[data-cy="error-message"]').should('not.exist');
+
     cy.get('[data-cy="email-input"]').type('wrongemail@gmail.com');
     cy.get('[data-cy="password-input"]').type('123456');
     cy.get('[data-cy="Log in"]').click();
-    cy.get('[data-cy="error-message"]').should('be.visible');
+
+    cy.get('[data-cy="error-message"]').should('contain.text', 'Error');
 
   })
 })
