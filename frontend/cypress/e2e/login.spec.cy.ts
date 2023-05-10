ae7e483046c59f9f5f2a3d79c78467750b0c3850
Jia Tee <jiateec@gmail.com>
2023-05-10 23:09:37 +1200

    PDF-146 - fix test names

--- frontend/cypress/e2e/login.spec.cy.ts
+++ frontend/cypress/e2e/login.spec.cy.ts
@@ -1,8 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Login functionality', () => {
+  it('Error logging in with wrong credentials', () => {
     cy.visit('http://localhost:5173/login')
     cy.get('[data-cy="error-message"]').should('not.exist');
-
     cy.get('[data-cy="email-input"]').type('wrongemail@gmail.com');
     cy.get('[data-cy="password-input"]').type('123456');
     cy.get('[data-cy="Log in"]').click();
