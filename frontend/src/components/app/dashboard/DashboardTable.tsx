1ff69c47eb56602ee49d5326ba377959a7d44416
Jia Tee <jiateec@gmail.com>
2023-05-11 16:56:13 +1200

    PDF-158 - style sort, format date, fix date sort

--- frontend/src/components/app/dashboard/DashboardTable.tsx
+++ frontend/src/components/app/dashboard/DashboardTable.tsx
@@ -8,6 +8,7 @@ import Tooltip from "../../Tooltip";
 import FileUploadModal from "./FileUploadModal";
 import { AuthContext } from "../../../contexts/AuthContextProvider";
 import {RecentContext} from "../../../contexts/RecentContextProvider";
+import moment from "moment";
 
 
 export interface DocumentRecord {
@@ -38,7 +39,7 @@ export default function DashboardTable({documentData} : Props) {
 
     const sortedData = useCallback(()=>{
         return documentData.sort((a, b) => {
-            if (sortDirection === "Ascending") {
+        if (sortDirection === "Ascending") {
                 if (a[sortKey] < b[sortKey]) {
                     return -1;
                 }
@@ -63,6 +64,11 @@ export default function DashboardTable({documentData} : Props) {
         navigate(`/document/${document.uuid}`);
     }
 
+    function formatLastUpdated(dateUTC: string) {
+        const localDate = moment.utc(dateUTC).local();
+        const formattedDate = localDate.format('D MMM YYYY [at] h:mm A');
+        return `${formattedDate}`;
+    }
 
     return(
         <>  
@@ -81,9 +87,9 @@ export default function DashboardTable({documentData} : Props) {
                 <table className="w-full text-sm text-left text-gray-500 dark:text-gray-200">
                     <thead className="text-lg text-gray-500 dark:text-gray-200">
                         <tr>
-                            <HeaderCell label="Name" setSortKey={setSortKey} setSortDirection={setSortDirection} sortDirection={sortDirection}/>
-                            <HeaderCell label="Owner" setSortKey={setSortKey} setSortDirection={setSortDirection} sortDirection={sortDirection}/>
-                            <HeaderCell label="Last updated" setSortKey={setSortKey} setSortDirection={setSortDirection} sortDirection={sortDirection}/>
+                            <HeaderCell label="Name" setSortKey={setSortKey} sortKey={"name"} setSortDirection={setSortDirection} sortDirection={sortDirection} isSelected={sortKey==="name"}/>
+                            <HeaderCell label="Owner" setSortKey={setSortKey} sortKey={"owner"} setSortDirection={setSortDirection} sortDirection={sortDirection} isSelected={sortKey==="owner"}/>
+                            <HeaderCell label="Last updated" setSortKey={setSortKey} sortKey={"lastUpdated"} setSortDirection={setSortDirection} sortDirection={sortDirection} isSelected={sortKey==="lastUpdated"}/>
                         </tr>
                     </thead>
                     <tbody>
@@ -95,7 +101,7 @@ export default function DashboardTable({documentData} : Props) {
                                     <button type="button" className="hover:underline" onClick={() =>  handleDocumentClicked(document)}>{document.name}</button>
                                 </td>
                                 <td className="py-3">{document.owner}</td>
-                                <td className="py-3">{document.lastUpdated}</td>
+                                <td className="py-3">{formatLastUpdated(document.lastUpdated)}</td>
                             </tr>
                         ))}
                     </tbody>
@@ -108,25 +114,25 @@ export default function DashboardTable({documentData} : Props) {
 interface HeaderCellProps {
     label: string,
     setSortKey: any,
+    sortKey: SortKeys,
     setSortDirection: any,
     sortDirection: SortOrder,
+    isSelected?: boolean
 }
 
+const selectedStyle = "text-gray-900 dark:text-gray-200";
 
-const HeaderCell = ({label, setSortKey, setSortDirection, sortDirection}: HeaderCellProps ) => {
+
+const HeaderCell = ({label, sortKey, setSortKey, setSortDirection, sortDirection, isSelected}: HeaderCellProps ) => {
+
     return (
         <>  
             <th scope="col" className="py-3">
-                <button type="button" onClick={() => { setSortKey(label.toLowerCase().replaceAll(" ","")); setSortDirection(sortDirection==="Ascending" ? "Descending": "Ascending")}} className="flex flex-row items-center gap-1">
+                <button type="button" onClick={() => { setSortKey(sortKey); setSortDirection(sortDirection==="Ascending" ? "Descending": "Ascending")}} className={`flex flex-row items-center gap-1 ${isSelected ? selectedStyle : ''}` }>
                     <span>{label}</span>
                     <ArrowsUpDownIcon className="w-5 h-5"/>
                 </button>
             </th>
         </>
     )
-}
-
-
-function uploadPDF() {
-    console.log("Upload PDF")
 }
