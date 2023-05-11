d7572cfc101afd7e6ebcf089e445ab796c58bff1
Matan Yosef <matan.yosef02@gmail.com>
2023-05-11 13:04:37 +1200

    PDF-113: Restyled core parts of upload

--- frontend/src/components/app/dashboard/FileUploadModal.tsx
+++ frontend/src/components/app/dashboard/FileUploadModal.tsx
@@ -1,18 +1,19 @@
-import {useContext, useState} from "react";
+import {useContext, useRef, useState} from "react";
 import Modal from "../../Modal";
-import UploadFileButton from "./UploadFileButton";
 import axios from "axios";
 import {useAuthState} from "react-firebase-hooks/auth";
 import {auth} from "../../../firebaseAuth";
 import {useNavigate} from "react-router-dom";
 import {AuthContext} from "../../../contexts/AuthContextProvider";
+import {DocumentArrowUpIcon} from "@heroicons/react/24/outline";
+import PrimaryButton from "../../PrimaryButton";
 
-interface FileUploadModalProps {
+interface Props {
     isVisible: boolean;
     onOutsideClick: (params: any) => any;
 }
 
-export default function FileUploadModal({isVisible, onOutsideClick}: FileUploadModalProps){
+export default function FileUploadModal({isVisible, onOutsideClick}: Props){
 
     const {firebaseUserRef} = useContext(AuthContext);
 
@@ -20,18 +21,25 @@ export default function FileUploadModal({isVisible, onOutsideClick}: FileUploadM
 
     const [uploadedFileName, setUploadedFileName] = useState<string>("No file selected");
 
-    const borderStyle = isDragOver ? "border-2 border-dashed border-blue" : "border-2 border-dotted";
+    const input = useRef<HTMLInputElement>(null);
+
+    const borderStyle = isDragOver ? "border-2 border-dashed border-blue-500" : "border-2 border-dotted";
+    const bgStyle = isDragOver ? "bg-blue-100" : "";
 
     let navigate = useNavigate();
 
     return (
         <Modal isVisible={isVisible} onOutsideClick={onOutsideClick}>
-            <div className="flex flex-col items-center w-[40vh] h-[40vh] p-4 dark:bg-anno-space-700 rounded-md gap-3">
-                {/* <h1 className="text-2xl font-bold dark:text-white">Upload PDF</h1> */}
-                <UploadFileButton onFileUpload={importFile}/>
-                <div className={"w-full h-full flex flex-col items-center justify-center " + borderStyle} onDragOver={dragOver} onDragLeave={dragLeave} onDrop={drop}>
-                    <p className="text-center dark:text-white">Drag and drop your PDF here</p>
-                    <p className="text-center dark:text-white">{uploadedFileName}</p>
+            <div className="flex flex-col items-center w-[60vw] h-[60vh] p-6 dark:bg-anno-space-700 rounded-md gap-3">
+                 <h1 className="text-2xl font-bold text-anno-red-primary dark:text-white self-start">Upload PDF</h1>
+                 <h1 className="dark:text-zinc-300 self-start"><span className="text-anno-red-secondary">* </span>Please select a PDF file to upload</h1>
+                <div className={`w-full h-full flex flex-col gap-2 items-center justify-center ${borderStyle} + ${bgStyle}`} onDragOver={dragOver} onDragLeave={dragLeave} onDrop={drop}>
+                    <DocumentArrowUpIcon className="w-24 h-24 text-zinc-500 dark:text-white" />
+
+                    <h1 className="text-2xl text-center dark:text-white">Drag & drop</h1>
+                    <p className="text-2xl text-center dark:text-white">or <button onClick={()=>input.current?.click()} className="underline underline-offset-4 text-anno-red-primary hover:text-anno-red-secondary transition-colors dark:text-anno-red-secondary hover:text-anno-red-primary">browse</button></p>
+                    <input type="file" id="file" name="file" onChange={handleFileUpload} accept="application/pdf" ref={input} className="hidden"/>
+                    <p className="text-center text-zinc-400 dark:text-white">{uploadedFileName}</p>
                 </div>
             </div>
         </Modal>
@@ -58,6 +66,11 @@ export default function FileUploadModal({isVisible, onOutsideClick}: FileUploadM
         
     }
 
+    function handleFileUpload(event: any) {
+        const file = event.target.files[0];
+        importFile(file);
+    }
+
     async function importFile(file: File) {
         setUploadedFileName(file.name);
 
@@ -72,6 +85,7 @@ export default function FileUploadModal({isVisible, onOutsideClick}: FileUploadM
                 // Success
                 navigate(`/document/${response.data.uuid}`)
         }).catch(error => {
+
             console.log(error);
         });
 
