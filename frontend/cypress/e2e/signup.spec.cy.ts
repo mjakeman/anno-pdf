b213b8a4ce3659206a4b4a9a4409645bf75d6ee8
Jia Tee <jiateec@gmail.com>
2023-05-12 18:45:24 +1200

    PDF-181 - signup test with cleanup

--- frontend/cypress/e2e/signup.spec.cy.ts
+++ frontend/cypress/e2e/signup.spec.cy.ts
@@ -1,8 +1,4 @@
 describe('Signup functionality', () => {
-    // afterEach(() => {
-    //     cy.logout();
-    // });
-
     const newRandomEmail = () => {
         const uuid = () => Cypress._.random(0, 1e6);
         const id = uuid();
@@ -11,15 +7,22 @@ describe('Signup functionality', () => {
         const email = `${testname}@${domain}`;
         return email;
     }
+    const email = newRandomEmail();
+
+    afterEach(() => {
+        cy.deleteUser(`${email}`);
+    });
 
     it('Error signing up with existing email', () => {
-    cy.visit('http://localhost:5173/signup');
-    cy.get('[data-cy="first-name-input"]').type('New1');
-    cy.get('[data-cy="last-name-input"]').type('User2');
-    cy.get('[data-cy="email-input"]').type(`${newRandomEmail()}`);
-    cy.get('[data-cy="password-input"]').type('password');
-    cy.get('[data-cy="Sign up"]').eq(1).click();
-    cy.url().should('equal', 'http://localhost:5173/dash');
+        cy.visit('http://localhost:5173/signup');
+        cy.get('[data-cy="first-name-input"]').type('New1');
+        cy.get('[data-cy="last-name-input"]').type('User2');
+        cy.get('[data-cy="email-input"]').type(`${email}`);
+        cy.get('[data-cy="password-input"]').type('password');
+        cy.get('[data-cy="Sign up"]').eq(1).click();
+        cy.wait(1000);
+        cy.url().should('equal', 'http://localhost:5173/dash');
+        cy.wait(2000);   
     });
 
 })
