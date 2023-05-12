ea1d3e71305cb11e009b4ca07bab0bb32615e212
Jia Tee <jiateec@gmail.com>
2023-05-12 12:15:32 +1200

    PDF-177 - use spinner component

--- frontend/src/components/app/dashboard/Dashboard.tsx
+++ frontend/src/components/app/dashboard/Dashboard.tsx
@@ -3,6 +3,7 @@ import {useContext, useEffect, useState} from "react";
 import axios from "axios";
 import {AuthContext} from "../../../contexts/AuthContextProvider";
 import { LoadedDocContext} from "../../../contexts/LoadedDocsContextProvider";
+import AnimatedSpinner from "../AnimatedSpinner";
 
 
 export default function Dashboard() {
@@ -47,14 +48,7 @@ export default function Dashboard() {
                     <DashboardTable documentData={documents}/>
                 :
                 <div className="grow grid place-items-center h-full w-full">
-                    <svg className="animate-spin h-16 w-16 text-blue-500" xmlns="http://www.w3.org/2000/svg"
-                         fill="none" viewBox="0 0 24 24">
-                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
-                                strokeWidth="4"></circle>
-                        <path className="opacity-75" fill="currentColor"
-                              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
-                        </path>
-                    </svg>
+                    <AnimatedSpinner className="h-16 w-16 text-blue-500"/>
                 </div>
             }
         </div>
