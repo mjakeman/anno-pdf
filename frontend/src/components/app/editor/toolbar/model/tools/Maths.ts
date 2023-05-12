cda29a86658688c40480f2888700e9aa0f94bc8d
Matan Yosef <matan.yosef02@gmail.com>
2023-05-12 22:54:50 +1200

    CT: Added comments for tool models

--- frontend/src/components/app/editor/toolbar/model/tools/Maths.ts
+++ frontend/src/components/app/editor/toolbar/model/tools/Maths.ts
@@ -3,6 +3,9 @@ import {Canvas, IObjectOptions, Object} from "fabric/fabric-impl";
 import {fabric} from "fabric";
 import TeXToSVG from "tex-to-svg";
 
+/**
+ * Tool Representation
+ */
 class Maths extends Tool {
 
     constructor(id: string)  {
@@ -11,6 +14,7 @@ class Maths extends Tool {
 
     draw(canvas: Canvas): void {
         canvas.on('mouse:down', (event) => {
+            // If we click the canvas (not an object):
             if (event.target === null) {
                 // @ts-ignore
                 let text = new MathAnnotation("\\frac{n!}{k!(n-k)!} = \\binom{n}{k}", {
@@ -26,7 +30,14 @@ class Maths extends Tool {
 }
 export default Maths;
 
+/**
+ * Math Annotation representation (used by Math Tool).
+ *
+ * Essentially this object is meant to create either an SVG (for the Maths presentation) or an IText (for the latex
+ * editing) - which allows for smooth 'Math Annotations'.
+ */
 export const MathAnnotation = fabric.util.createClass(fabric.Object, {
+
     type: 'MathItext',
     latex: '',
     svgString: '',
@@ -34,20 +45,38 @@ export const MathAnnotation = fabric.util.createClass(fabric.Object, {
     initialize: function (initialLatex: string, options: IObjectOptions) {
         options = options || {};
         this.latex = initialLatex;
+
+        // Get the latex as an svg
         this.svgString = TeXToSVG(this.latex);
+
+        // Initialise this as a fabric object
         this.callSuper('initialize', options);
-        this.on('mousedblclick', this.edit);
+
+        // Create a listener to allow for editing
+        this.on('mousedblclick', this.editLatex);
     },
 
+    /**
+     * This renders the svg on a given canvas.
+     * When this object is initially rendered, it is an SVG.
+     * @param svgString
+     * @param canvas
+     */
     _renderMath(svgString: string, canvas: fabric.Canvas) {
-        // Render Math object
+
+        // Keep a reference of the object
         let current = this;
-        console.log(`at stage a: ${(current as any).uuid}`);
+
+        // Load the SVG from the given string
         fabric.loadSVGFromString(svgString, (objects, options) => {
             let obj = fabric.util.groupSVGElements(objects, options);
             (obj as any).set({latex: this.latex});
-            obj.scale(10);
+            obj.scale(10); // svg is small - so scale
+
+
             obj.on('mousedblclick', (e) => this.editLatex(obj.canvas, obj, (obj as any).latex))
+
+            // Set object fields needed for a canvas object
             let left =   current.left;
             let top =    current.top;
             let angle = current.angle;
@@ -56,20 +85,17 @@ export const MathAnnotation = fabric.util.createClass(fabric.Object, {
                 top: top,
                 angle: angle,
             });
+
             // @ts-ignore
             obj['uuid'] = current['uuid'];
-            console.log(`at stage b: ${(obj as any).uuid}`);
             obj.toObject = (function(objRef, toObject) {
                 return () => {
-                    console.log("to objecting");
-                    console.log(objRef);
                     const intermediary = fabric.util.object.extend(toObject.call(objRef), {
                         uuid: (objRef as any).uuid,
                         latex: (objRef as any).latex
                     });
                     let mathAnnotation = new MathAnnotation("\\frac{n!}{k!(n-k)!} = \\binom{n}{k}", intermediary.options);
                     mathAnnotation.uuid = intermediary.uuid;
-                    console.log(mathAnnotation);
                     return mathAnnotation.toObject(['uuid', 'latex']);
                 };
             })(obj, obj.toObject);
@@ -80,6 +106,7 @@ export const MathAnnotation = fabric.util.createClass(fabric.Object, {
         });
     },
 
+    // Override the object render method
     _render: function (ctx: CanvasRenderingContext2D) {
         this._renderMath(this.svgString, this.canvas);
     },
