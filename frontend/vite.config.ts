81b68da3e311faec89566117e9c9406af87834da
Jia Tee <jiateec@gmail.com>
2023-05-12 21:52:50 +1200

    PDF-200 - remove unused vitest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,4 @@
 /// <reference types="vite/client" />
-/// <reference types="vitest" />
 
 
 
@@ -10,10 +9,4 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    threads: false, // Threads disabled to fix canvas error
-    setupFiles: './src/setupTests.ts',
-  },
 });
