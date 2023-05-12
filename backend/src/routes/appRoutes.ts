5678890fe092e8fe551d03ffc363bc3457de32d7
OjasMadaan <ojasmadaan@gmail.com>
2023-05-12 13:40:23 +1200

    Update appRoutes.ts

--- backend/src/routes/appRoutes.ts
+++ backend/src/routes/appRoutes.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import userRouter from '../routes/api/users'
 import documentRouter from '../routes/api/documents'
-import middleware from "../firebase/middleware";
+import {validateToken} from "../firebase/middleware";
 
 /*
  * Main routing file to manage all application routes.
  */
 const router = Router();
 
-router.use(['/users', '/user'], middleware.validateToken, userRouter);
-router.use('/documents', middleware.validateToken, documentRouter);
+router.use(['/users', '/user'], validateToken, userRouter);
+router.use('/documents', validateToken, documentRouter);
 
 export default router;
