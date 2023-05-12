3127298ebab9e30ad8e3f1554d576eb5e86563f9
Jordan York <yorkjordan07@gmail.com>
2023-05-12 13:38:21 +1200

    PDF-182 - Toast position adjusted to only be bottom-centre

--- frontend/src/hooks/useToast.tsx
+++ frontend/src/hooks/useToast.tsx
@@ -3,25 +3,20 @@ import { useState } from "react";
 import { FC } from "react";
 import { createContext } from "react";
 import Toast from "../components/Toast";
-import { ToastContextType, ToastPoisition, toastPositionsMap, ToastProps } from "../types/ToastUtils";
+import { ToastContextType, ToastPoisition, ToastProps } from "../types/ToastUtils";
 
 
 export const ToastContext = createContext<ToastContextType>({
     addToast: () => {},
     remove: () => {},
-    position: "bottom-right"
 });
 
 export const useToast = () => useContext(ToastContext);
 
 export const ToastProvider: FC <({children: ReactNode})> = ({children}) => {
     const [toastList, setToastList] = useState<ToastProps[]>([]);
-    const [position, setPosition] = useState<ToastPoisition>("bottom-middle");
 
     const add = (toast: Omit<ToastProps, "id">) => {
-        if(toast.position){
-            setPosition(toast.position)
-        }
         // Append toast to list 
         setToastList((toastList) => [...toastList, { ...toast, id: Math.random()*100000}]);
     }
@@ -35,9 +30,9 @@ export const ToastProvider: FC <({children: ReactNode})> = ({children}) => {
 
     return (
         <div className="">
-            <ToastContext.Provider value={{addToast: add, remove, position}}>
+            <ToastContext.Provider value={{addToast: add, remove}}>
                 {children}
-                <div className={"fixed z-[9999] w-screen max-w-xs m-3 "+toastPositionsMap[position] }>
+                <div className={"fixed z-[9999] w-screen max-w-xs m-3 bottom-0 left-1/2 transform -translate-x-1/2 right-1/2" }>
                     {toastList.map((toast)=> (
                         <Toast key={toast.id} {...toast}/>
                     ))}
